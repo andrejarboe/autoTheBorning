837641ca1478603d980aa77ca06d97369867cb5c
andrejarboe <andrejarboe@gmail.com>
2022-07-24 19:19:19 -0400

    finish hello.py

--- hello.py
+++ hello.py
@@ -1,8 +1,15 @@
 print('Hello world!')
+
+#
 print('What is your name?')
 myName= input()
 print('It its good to meet you, ' + myName)
 print('The length oif your name is: ')
-print(len(myName))
-print()
+print(len(myName)) 
+
+#
+print('What is your age?') #ask for their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.') 
+
 
