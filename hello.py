ccc0396441712dee6828924d78ba3d60a6daf421
andrejarboe <andrejarboe@gmail.com>
2022-07-24 19:15:18 -0400

    first save

---
+++ hello.py
@@ -0,0 +1,8 @@
+print('Hello world!')
+print('What is your name?')
+myName= input()
+print('It its good to meet you, ' + myName)
+print('The length oif your name is: ')
+print(len(myName))
+print()
+
