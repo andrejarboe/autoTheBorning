0aac1a9e586da0928cee26c5027b171b70306efb
Andre Jarboe <andrejarboe@gmail.com>
2022-08-07 15:15:55 -0400

    functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+def hello(name):
+    print('Howdy!')
+    print('Howdy!!!')
+    print('Hello there ' +name)
+
+hello('Andre')
+hello('Alice')
+hello('Bob')
+
+print('Hello has ' +str(len('hello'))+ ' letters in it.')
