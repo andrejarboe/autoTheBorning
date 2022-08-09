4164c5ab621fad8a16340514a657a2ad2514512d
Andre Jarboe <andrejarboe@gmail.com>
2022-08-08 23:39:46 -0400

    made tic tac toe board

---
+++ tictactoe.py
@@ -0,0 +1,11 @@
+#Tic-Tac-Toe
+
+import pprint
+
+theBoard = {
+	'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+	'mid-L': ' ', 'mid-M': 'X', 'mid-R': ' ',
+	'low-L': ' ', 'low-M': ' ', 'low-R': ' ',
+} 
+
+pprint.pprint(theBoard)
