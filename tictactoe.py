3290055cbf386376cace0e92adc2fab03d312095
Andre Jarboe <andrejarboe@gmail.com>
2022-08-08 23:46:12 -0400

    print the board

--- tictactoe.py
+++ tictactoe.py
@@ -8,4 +8,11 @@ theBoard = {
 	'low-L': ' ', 'low-M': ' ', 'low-R': ' ',
 } 
 
-pprint.pprint(theBoard)
+def printBoard(board):
+	print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'] )
+	print('-+-+-')
+	print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+	print('-+-+-')
+	print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+printBoard(theBoard)
