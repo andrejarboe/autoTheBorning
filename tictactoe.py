29b828ec7180b3ad9ed793cd3c196dc7fe1f771e
Andre Jarboe <andrejarboe@gmail.com>
2022-08-09 01:13:11 -0400

    refactor to make more game like

--- tictactoe.py
+++ tictactoe.py
@@ -1,50 +1,91 @@
 #Tic-Tac-Toe
 
-import pprint, random
+#import pprint, random
 
-theBoard = {
+#theBoard = {
+#	'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+#	'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+#	'low-L': ' ', 'low-M': ' ', 'low-R': ' ',
+#} 
+
+#def printBoard(board):
+#	print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'] )
+#	print('-+-+-')
+#	print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+#	print('-+-+-')
+#	print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
+
+#def userMove(move):
+#	if theBoard[move] == ' ':
+#		theBoard[move] = 'X'
+	
+	
+
+#print('Lets flip a coin to see who goes first')
+#coin = random.randint(1,2)
+#print('Select 1 for Heads or 2: for Tails')
+
+#print('DEBUG: coin is: ' +str(coin))
+
+
+#guess = int(input())
+#if guess == coin:
+#	turn = 'X'
+#	print('Darn you guessed correct.')
+#	print('You go first.')
+#else:
+#	turn = 'O'
+#	print('Sorry, you guessed wrong.')
+#	print('''I'll go first.''')
+
+
+#printBoard(theBoard)
+#for i in range(9):
+#	if turn == 'X':
+#		print('make a move (top-, mid-, low- & L, M, R)')
+#		#take input
+#		move = input()
+#		# add x
+#		userMove(move)
+#		printBoard(theBoard)
+
+#sudo code 
+# board
+# display Board
+# start game
+# check win
+	#check rows
+	#check columns
+	#check diagonals
+# check tie
+# togle player turn 
+
+#board
+board = {
 	'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
 	'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
 	'low-L': ' ', 'low-M': ' ', 'low-R': ' ',
 } 
 
-def printBoard(board):
+#display board
+def displayBoard():
 	print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'] )
 	print('-+-+-')
 	print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
 	print('-+-+-')
 	print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
-def userMove(move):
-	if theBoard[move] == ' ':
-		theBoard[move] = 'X'
-	
-	
-
-print('Lets flip a coin to see who goes first')
-coin = random.randint(1,2)
-print('Select 1 for Heads or 2: for Tails')
-
-print('DEBUG: coin is: ' +str(coin))
+#play game
+def playGame():
+	displayBoard()
+	handleTurn()
 
 
-guess = int(input())
-if guess == coin:
-	turn = 'X'
-	print('Darn you guessed correct.')
-	print('You go first.')
-else:
-	turn = 'O'
-	print('Sorry, you guessed wrong.')
-	print('''I'll go first.''')
+def handleTurn():
+	position = input('make a move (top-, mid-, low- & L, M, R)')
+	board[position] = 'X'
+	displayBoard()
 
+#the game
+playGame()
 
-printBoard(theBoard)
-for i in range(9):
-	if turn == 'X':
-		print('make a move (top-, mid-, low- & L, M, R)')
-		#take input
-		move = input()
-		# add x
-		userMove(move)
-		printBoard(theBoard)
