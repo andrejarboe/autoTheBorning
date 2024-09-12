af9d352bd757d4a4d68824fbc1105f61c3c1cb5f
andrejarboe <andrejarboe@gmail.com>
2024-09-12 09:25:38 -0500

    save

---
+++ files/filePaths.py
@@ -0,0 +1,7 @@
+import os
+file = os.path.join('folder1','folder2','folder3','file.png')
+
+print(file)
+
+# get current working directory
+print(os.getcwd())
--- guessTheNum.py
+++ guessTheNum.py
@@ -4,24 +4,26 @@ import random
 print('Hello what is your name?')
 name = input()
 
-print('Well, ' +name+ ', I am thining of a number between 1 and 20.')
-secrectNumber = random.randint(1, 20)
+print('Well, ' +name+ ', I am thinking of a number between 1 and 20.')
+secretNumber = random.randint(1, 20)
 print('Take a guess...')
 
 for guessTaken in range(1, 7):
     guess = int(input())
 
-    if guess < secrectNumber:
+    if guess < secretNumber:
         print('Your guess is too low')
-    elif guess > secrectNumber:
+    elif guess > secretNumber:
         print('Your guess is too high.')
     else:
         break # tis sis for a correct guess
 
-if guess == secrectNumber:
+if guess == secretNumber:
     print('Good Job, ' +name+ '!!!')
     print('You guessed my number in ' +str(guessTaken)+ ' guesses.')
 else:
-    print('Nope. The number I was thinking of was ' +str(secrectNumber)+ '...')
+    print('Nope. The number I was thinking of was ' +str(secretNumber)+ '...')
     print('You used all ' +str(guessTaken)+ ' of yor guesses.')
     print('************GAME OVER*************')
+
+input("")
